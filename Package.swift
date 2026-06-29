977f5b5e7f8d46b6395a9afb3449af5fb9128e74
tyagishi <tyagishi@gmail.com>
2026-06-29 09:45:52 +0900

    F - bump to iOS17

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "SDSStoreKitStore",
     platforms: [
         .macOS(.v14),
-        .iOS(.v16),
+        .iOS(.v17),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
